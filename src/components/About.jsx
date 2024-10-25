15843276e68e90d61f4c201d6dc6876fc60127f6
AdityaRanjan2005 <103ranjanadityakishu@gmail.com>
2024-10-26 01:13:15 +0530

    initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Hello! Proficient in MERN stack and API development, I've earned recognition as a finalist at the prestigious GOI DPBH Hackathon'23, reflecting my deep involvement in web development. Additionally, I've achieved 5-Star Hackerrank Badges in Python and SQL, showcasing my expertise in data-driven solutions. Delving into blockchain technology has further fueled my passion for innovation. Currently, I'm excitedly spearheading CohortLab, a groundbreaking project aimed at transforming collaborative workspaces. My dedication to continuous learning drives me to push the boundaries of what's possible in the digital realm
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,265 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  cohortlab,
+  figma,
+  docker,
+  meta,
+  swapso,
+  edxtra,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "MERN Stack Developer",
+    icon: web,
+  },
+  {
+    title: "Devops",
+    icon: mobile,
+  },
+  {
+    title: "Blockchain Developer",
+    icon: backend,
+  },
+  {
+    title: "SEO specialist",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Founder",
+    company_name: "CohortLab",
+    icon: cohortlab,
+    iconBg: "#383E56",
+    date: "January 2024 - Present",
+    points: [
+      "Driving Technological Innovation: Spearheading the adoption of cutting-edge solutions to enhance collaborative learning experiences.",
+
+      "Leading Product Development: Overseeing the entire product lifecycle, ensuring alignment with the company's vision while fostering innovation within the technical team.",
+      
+      "Building a Skilled Technical Team: Nurturing a culture of excellence and collaboration, assembling a talented team capable of delivering on CohortLab's ambitious goals.",
+      
+      "Prioritizing Scalability and Security: Implementing robust infrastructure and cybersecurity measures to accommodate growth and safeguard user data, ensuring CohortLab's platform remains reliable and secure.",
+    ],
+  },
+  {
+    title: "Program Director",
+    company_name: "Swapso.io",
+    icon: swapso,
+    iconBg: "#E6DEDD",
+    date: "August 2023 - Present",
+    points: [
+      "Key Contributor to SwapSo.io: Played a significant role in SwapSo.io, contributing to its tech initiatives and program management, leveraging skills in program coordination and technology to advance its educational goals.",
+
+      "Campus Ambassador to Head of Campus Ambassadors: Demonstrated exceptional leadership and technical expertise, progressing from a Campus Ambassador role to Head of Campus Ambassadors within just five months, showcasing effective team building and management skills.",
+      
+      "Driving Technological Innovation: Implemented innovative solutions and strategies within SwapSo.io, leveraging expertise in program management and technology to enhance its platform and initiatives.",
+      
+      "Hybrid Work Model Implementation: Successfully adapted to a hybrid work model, balancing remote and on-site responsibilities effectively, showcasing flexibility and adaptability in managing tech projects and program coordination at SwapSo.io.",
+    ],
+  },
+  {
+    title: "Product Manager",
+    company_name: "edXtra",
+    icon: edxtra,
+    iconBg: "#E6DEDD",
+    date: "August 2024 - Present",
+    points: [
+      "Product Optimization & Issue Resolution: Spearheaded bug identification and resolution, significantly improving platform stability and user experience. Successfully tackled high-impact issues, leading to a 30% reduction in reported issues and enhancing overall platform reliability.",
+
+      "Cross-Functional Project Management: Collaborated closely with design, engineering, and marketing teams to implement high-quality websites and digital solutions for program execution. Leveraged version control and comprehensive logging to streamline development processes, leading to efficient and transparent project workflows.",
+      
+      "User Engagement & Growth: Developed and implemented user-centric management strategies, resulting in a 25% increase in active user engagement. Facilitated product enhancements that directly contributed to a 20% rise in user retention.",
+      
+      "Product Strategy & Vision: Played a pivotal role in shaping EdXtra's product roadmap, aligning features and updates with user needs and market trends. Provided valuable insights and strategic guidance, helping to solidify EdXtra’s position in the EdTech industry and expand its user base effectively.",
+    ],
+  },
+  
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Aditya proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Aditya does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Aditya optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/adityaranjan2005",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/adityaranjan2005",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/adityaranjan2005",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
